257eea18cccbab58176ebf5919b8837e7cde300b
sunnan.1121 <sunnan.1121@bytedance.com>
2022-09-26 20:13:19 +0800

    [feature] tips

--- src/main/kotlin/flyer/utils/KryoUtils.kt
+++ src/main/kotlin/flyer/utils/KryoUtils.kt
@@ -9,9 +9,7 @@ object KryoUtils {
     private val kryoPool = object : Pool<Kryo>(true, false, 128) {
         override fun create(): Kryo {
             val kryo = Kryo()
-            // 关闭序列化注册，会导致性能些许下降，但在分布式环境中，注册类生成ID不一致会导致错误
             kryo.isRegistrationRequired = false
-            // 支持循环引用，也会导致性能些许下降 T_T
             kryo.references = true
             return kryo
         }
--- src/test/kotlin/test/t5.kt
+++ src/test/kotlin/test/t5.kt
@@ -1,44 +1,88 @@
 package test
 
-class t5 {
-}
+import test.StringUtils.STARLING.buildStringWithTemplate
 
-/**
- * replace single key with value in template string
- * @param template template string
- * @param key replaced string in original template
- * @param value expected string in new template
- */
-fun buildStringWithTemplate(template: String, key: String, value: String): String {
-    val index = template.indexOf(key)
-    if (index == -1) {
-       println("[tiktokec_template_key_not_found] template = $template, key = $key")
-    }
-    return template.replace(key, value)
-}
 
-/**
- * replace keys with respective values in template string
- * @param template template string
- * @param map all <key,value> pairs
- */
-fun buildStringWithTemplate(template: String, map: HashMap<String, String>, mode: String = "native"): String {
-    var afterTemplate = template
-    for (pair in map) {
-        if (template.indexOf(pair.key) == -1) {
-            println("[tiktokec_template_key_not_found] template = $template, key = ${pair.key}")
-        } else {
-            afterTemplate = afterTemplate.replace(pair.key, pair.value)
+object StringUtils {
+
+    object STARLING {
+        /**
+         * replace single key with value in template string
+         * @param template template string
+         * @param key replaced string in original template
+         * @param value expected string in new template
+         */
+        fun buildStringWithTemplate(template: String, key: String, value: String): String {
+            val index = template.indexOf(key)
+            if (index == -1) {
+                println("[tiktokec_template_key_not_found] template = $template, key = $key")
+            }
+            return template.replace(key, value)
         }
+
+        /**
+         * replace keys with respective values in template string
+         * @param template template string
+         * @param map all <key,value> pairs
+         */
+        fun buildStringWithTemplate(template: String, map: HashMap<String, String>): String {
+            val afterTemplate = StringBuilder(template)
+            for (pair in map) {
+                if (template.indexOf(pair.key) == -1) {
+                    println("[tiktokec_template_key_not_found] template = $template, key = ${pair.key}")
+                } else {
+                    afterTemplate.replaceAll(pair.key, pair.value)
+                }
+            }
+            return afterTemplate.toString()
+        }
+
+        /**
+         * replace keys with respective values in template stringBuilder
+         * @param template template stringBuilder
+         * @param map all <key,value> pairs
+         */
+        fun buildStringWithTemplate(template: StringBuilder, map: HashMap<String, String>): StringBuilder {
+            for (pair in map) {
+                if (template.indexOf(pair.key) == -1) {
+                    println("[tiktokec_template_key_not_found] template = $template, key = ${pair.key}")
+                } else {
+                    template.replaceAll(pair.key, pair.value)
+                }
+            }
+            return template
+        }
+
+
+        private fun StringBuilder.replaceAll(oldValue: String, newValue: String) {
+            if (oldValue.isNullOrEmpty() || newValue.isNullOrEmpty())
+                return
+            var index = this.indexOf(oldValue)
+            if (index > -1 && oldValue != newValue) {
+                var lastIndex = 0
+                while (index > -1) {
+                    this.replace(index, index + oldValue.length, newValue)
+                    lastIndex = index + newValue.length
+                    index = this.indexOf(oldValue, lastIndex)
+                }
+            }
+        }
+
+
     }
-    return afterTemplate
+
 }
+
+
 
 fun main() {
-    val str = "this is a example \${key} string \${key}"
-    println(buildStringWithTemplate(str, hashMapOf(
-        Pair("1","1"),
-        Pair("\${key}", "value")
-    ))
+    val str = "this is a example \${key} string \${key} \${key2}"
+    val builder = StringBuilder(str)
+    println(buildStringWithTemplate(builder, hashMapOf(
+        "\${key}" to "value",
+        "1" to "2",
+        "\${key2}" to "value2"
     )
+
+    ).toString())
 }
